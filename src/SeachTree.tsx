f819690cb6e7bc98c27e1e18e895ea8514099565
Bình Nhữ <binh.thanhnhu@gmail.com>
2021-09-25 18:15:14 +0700

    searchable and draggable non-data

--- src/SeachTree.tsx
+++ src/SeachTree.tsx
@@ -6,13 +6,9 @@ const { Search } = Input;
 const x = 3;
 const y = 2;
 const z = 1;
-const gData: { title: string; key: string; children: any[] }[] = [];
+const gData: any = [];
 
-const generateData = (
-  _level: number,
-  _preKey?: string,
-  _tns?: { title: string; key: string; children?: any[] }[]
-) => {
+const generateData = (_level: any, _preKey?: any, _tns?: any) => {
   const preKey = _preKey || "0";
   const tns = _tns || gData;
 
@@ -35,10 +31,8 @@ const generateData = (
 };
 generateData(z);
 
-const dataList: { title: string; key: string }[] = [];
-const generateList = (
-  data: { title: string; key: string; children?: any[] }[]
-) => {
+const dataList: any = [];
+const generateList = (data: any) => {
   for (let i = 0; i < data.length; i++) {
     const node = data[i];
     const { key } = node;
@@ -66,33 +60,45 @@ const getParentKey = (key: any, tree: any): any => {
 };
 
 const SearchTree = () => {
-  const [expandedKeys, setExpandedKeys] = useState<string[]>([]);
+  const [expandedKeys, setExpandedKeys] = useState<any>([]);
   const [searchValue, setSearchValue] = useState([]);
   const [autoExpandParent, setAutoExpandParent] = useState<boolean>(true);
+  const [_gData, setGData] = useState<any>(gData);
 
-  const onExpand = (expandedKeys: any) => {
-    setExpandedKeys(expandedKeys);
-    setAutoExpandParent(false);
+  const onDragEnter = (info: any) => {
+    console.log(info);
+    // expandedKeys 需要受控时设置
+    // this.setState({
+    //   expandedKeys: info.expandedKeys,
+    // });
+    setExpandedKeys(info.expandedKeys);
   };
 
   const onChange = (e: any) => {
     const { value } = e.target;
     const expandedKeys = dataList
-      .map((item) => {
+      .map((item: any) => {
         if (item.title.indexOf(value) > -1) {
-          return getParentKey(item.key, gData);
+          return getParentKey(item.key, _gData);
         }
         return null;
       })
-      .filter((item, i, self) => item && self.indexOf(item) === i);
+      .filter(
+        (item: any, i: any, self: any) => item && self.indexOf(item) === i
+      );
 
     setExpandedKeys(expandedKeys);
     setSearchValue(value);
     setAutoExpandParent(true);
   };
 
-  const loop = (data: any[]): any =>
-    data.map((item) => {
+  const onExpand = (expandedKeys: any) => {
+    setExpandedKeys(expandedKeys);
+    setAutoExpandParent(false);
+  };
+
+  const loop = (data: any) =>
+    data.map((item: any) => {
       const index = item.title.indexOf(searchValue);
       const beforeStr = item.title.substr(0, index);
       const afterStr = item.title.substr(index + searchValue.length);
@@ -116,18 +122,87 @@ const SearchTree = () => {
       };
     });
 
+  const onDrop = (info: any) => {
+    console.log(info);
+    const dropKey = info.node.key;
+    const dragKey = info.dragNode.key;
+    const dropPos = info.node.pos.split("-");
+    const dropPosition =
+      info.dropPosition - Number(dropPos[dropPos.length - 1]);
+
+    const loop = (data: any, key: any, callback: any) => {
+      for (let i = 0; i < data.length; i++) {
+        if (data[i].key === key) {
+          return callback(data[i], i, data);
+        }
+        if (data[i].children) {
+          loop(data[i].children, key, callback);
+        }
+      }
+    };
+    const data = [..._gData];
+
+    // Find dragObject
+    let dragObj: any;
+    loop(data, dragKey, (item: any, index: any, arr: any) => {
+      arr.splice(index, 1);
+      dragObj = item;
+    });
+
+    if (!info.dropToGap) {
+      // Drop on the content
+      loop(data, dropKey, (item: any) => {
+        item.children = item.children || [];
+        // where to insert 示例添加到头部，可以是随意位置
+        item.children.unshift(dragObj);
+      });
+    } else if (
+      (info.node.props.children || []).length > 0 && // Has children
+      info.node.props.expanded && // Is expanded
+      dropPosition === 1 // On the bottom gap
+    ) {
+      loop(data, dropKey, (item: any) => {
+        item.children = item.children || [];
+        // where to insert 示例添加到头部，可以是随意位置
+        item.children.unshift(dragObj);
+        // in previous version, we use item.children.push(dragObj) to insert the
+        // item to the tail of the children
+      });
+    } else {
+      let ar: any;
+      let i: any;
+      loop(data, dropKey, (item: any, index: any, arr: any) => {
+        ar = arr;
+        i = index;
+      });
+      if (dropPosition === -1) {
+        ar.splice(i, 0, dragObj);
+      } else {
+        ar.splice(i + 1, 0, dragObj);
+      }
+    }
+
+    setGData(data);
+  };
+
   return (
-    <div className="search-tree">
+    <div className="search-and-result">
       <Search
-        style={{ marginBottom: 8, width: 400 }}
+        style={{ marginBottom: 8 }}
         placeholder="Search"
         onChange={onChange}
       />
       <Tree
+        className="draggable-tree"
+        defaultExpandedKeys={expandedKeys}
         onExpand={onExpand}
         expandedKeys={expandedKeys}
         autoExpandParent={autoExpandParent}
-        treeData={loop(gData)}
+        draggable
+        blockNode
+        onDragEnter={onDragEnter}
+        onDrop={onDrop}
+        treeData={loop(_gData)}
       />
     </div>
   );
